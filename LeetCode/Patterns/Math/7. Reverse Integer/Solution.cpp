92fb255c11c5ef53c66eed2977c2d2b1fde20836
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-22 13:58:15 +0530

    Add C++ Solution to Patterns/Math

---
+++ LeetCode/Patterns/Math/7. Reverse Integer/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int b;
+        int sum =0;
+        while(x != 0){
+            b = x % 10;
+             if ((sum > INT_MAX/10) || (sum < INT_MIN/10)){
+            return 0;
+            }
+            sum = (sum*10) + b;
+            x = x/10;
+           
+        
+        }
+        return sum;
+    }
+};
