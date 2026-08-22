26d15f2e0fd094dced58f2741f06b7a9c43fadbf
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-22 21:29:33 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/136. Single Number/Solution.java
+++ LeetCode/Java/Easy/136. Single Number/Solution.java
@@ -1,10 +1,5 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int count =0;
-        // int ans = 0;
-        for(int i = 0;i<nums.length;i++){
-            count = count ^ nums[i];
-        }
-        return count;
+        
     }
 }
