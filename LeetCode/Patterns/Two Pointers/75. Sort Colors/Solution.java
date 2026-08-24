d213b55d75b57ff03ab6fd12eceb83f54aeb9c3c
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-24 22:22:29 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/75. Sort Colors/Solution.java
+++ LeetCode/Patterns/Two Pointers/75. Sort Colors/Solution.java
@@ -1,5 +1,15 @@
 class Solution {
     public void sortColors(int[] nums) {
+        int temp =0;
+        for(int i = nums.length-1;i >= 1;i--){
+            for(int j = 0; j < i;j++){
+            if(nums[j] > nums[j+1]){
+                temp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = temp;
+            }
+        }
+    }
         
     }
 }
