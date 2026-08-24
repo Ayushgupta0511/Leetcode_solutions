a2d8b6116a0bc0104b5af51fd991d70e5f2e282f
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-24 11:08:50 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/383. Ransom Note/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/383. Ransom Note/Solution.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character , Integer> ch = new HashMap<>();
-        HashMap<Character , Integer> ex = new HashMap<>();
+        HashMap<Character , Integer> map1 = new HashMap<>();
+        HashMap<Character , Integer> map2 = new HashMap<>();
 
-        for(int i =0; i<magazine.length();i++){
-            char element = magazine.charAt(i);
-            ch.put(element , ch.getOrDefault(element , 0) + 1);
+        for(int i =0;i<magazine.length();i++){
+            char ch1 = magazine.charAt(i);
+
+            map1.put(ch1 , map1.getOrDefault(ch1 , 0)+1);
         }
-        for(int j =0;j < ransomNote.length();j++){
-            char ex2 = ransomNote.charAt(j);
-            ex.put(ex2 , ex.getOrDefault(ex2 , 0) + 1);
+        for(int i =0;i<ransomNote.length();i++){
+            char ch2 = ransomNote.charAt(i);
+
+            map2.put(ch2 , map2.getOrDefault(ch2 , 0) +1);
         }
-        for (Character key : ex.keySet()){
-            if(ch.getOrDefault(key , 0) < ex.get(key)){
-                return false;
+        for( Character key : map2.keySet()){
+            if(map2.get(key) > map1.getOrDefault(key , 0)){
+            return false;
             }
         }
+        
         return true;
     }
 }
