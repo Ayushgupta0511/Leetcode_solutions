032194cb9f05dee76aa91a11964627cf2ac42f4f
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-24 14:39:58 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/121. Best Time to Buy and Sell Stock/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/121. Best Time to Buy and Sell Stock/Solution.java
@@ -3,16 +3,18 @@ class Solution {
         int minprice = Integer.MAX_VALUE;
         int ans = Integer.MIN_VALUE;
         int profit =0;
+        int index  =0 ;
         for(int i = 0 ; i <prices.length;i++){
             if(prices[i] < minprice){
                 minprice = prices[i];
             }
+                index = prices[i];
                 profit = prices[i] - minprice;
                 if(profit > ans){
                     ans = profit;
                 }
         }
-        if(minprice == prices[0]){
+        if(index == prices[0]){
             ans = 0;
         }
         return ans;
