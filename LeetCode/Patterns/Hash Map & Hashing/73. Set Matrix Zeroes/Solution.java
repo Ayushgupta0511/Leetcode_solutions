d81f116ec470fe03dbe6d30b92c1d69121242eba
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-25 13:28:55 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/73. Set Matrix Zeroes/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/73. Set Matrix Zeroes/Solution.java
@@ -2,6 +2,7 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         HashSet <Integer> rows = new HashSet<>();
         HashSet <Integer> column = new HashSet<>();
+        int target =0;
         for(int i =0;i<matrix.length;i++){
             for(int j=0;j<matrix[i].length;j++){
                 if(matrix[i][j] == 0){
@@ -11,9 +12,11 @@ class Solution {
             }
         }
         for(int k : rows){
-            for(int j=0;j<matrix.length;j++){
+            for(int j=0;j<matrix[k].length;j++){
                 matrix[k][j] = 0;
+                target = k;
             }
+            
         }
         for(int l : column){
             for(int j=0;j<matrix.length;j++){
