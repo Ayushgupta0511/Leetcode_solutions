d613203fc4704543b44cd5d386acc4b090f69de4
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-25 13:05:12 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/73. Set Matrix Zeroes/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        HashSet <Integer> rows = new HashSet<>();
+        HashSet <Integer> column = new HashSet<>();
+        for(int i =0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j] == 0){
+                    rows.add(i);
+                    column.add(j);
+                }
+            }
+        }
+        for(int k : rows){
+            for(int j=0;j<matrix.length;j++){
+                matrix[k][j] = 0;
+            }
+        }
+        for(int l : column){
+            for(int j=0;j<matrix.length;j++){
+                matrix[j][l] = 0;
+            }
+        }
+    }
+}
