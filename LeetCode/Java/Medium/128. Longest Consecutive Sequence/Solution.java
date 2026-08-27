ff73cb16aab37f850775e63c26f448d6e9eb3f89
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-27 18:22:17 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/128. Longest Consecutive Sequence/Solution.java
+++ LeetCode/Java/Medium/128. Longest Consecutive Sequence/Solution.java
@@ -1,5 +1,28 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        
+        HashSet<Integer> Map = new HashSet<>();
+        int num =0;
+        int count =1;
+        int max =0;
+        for(int i =0;i<nums.length;i++){
+            Map.add(nums[i]);
+        }
+        for (int i = 0; i < nums.length; i++) {
+
+    num = nums[i];
+
+    if (!Map.contains(num - 1)) {
+
+        count = 1;
+
+        while (Map.contains(num + 1)) {
+            num++;
+            count++;
+        }
+
+        max = Math.max(max, count);
+    }
+}
+        return max;
     }
 }
