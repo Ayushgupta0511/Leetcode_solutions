4169b2311447fe727bdf5598158d034b803e219c
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-28 21:44:19 +0530

    Add Medium Java Solution for 2149. Rearrange Array Elements by Sign from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/2149. Rearrange Array Elements by Sign/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        int[] array = new int[nums.length];
+        int even =0;
+        int odd=1;
+        for(int i =0;i<nums.length;i++){
+                if(nums[i] > 0){
+                    array[even] = nums[i];
+                    even = even+2;
+                }
+                else{
+                    array[odd] = nums[i];
+                    odd = odd+2;
+            }
+        }
+    return array;
+    }
+}
