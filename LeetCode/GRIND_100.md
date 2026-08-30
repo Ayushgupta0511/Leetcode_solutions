74f62b03cfdd6358df1957253a7e038de0d7ddc9
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-30 14:59:53 +0530

    Add Medium Java Solution for 15. 3Sum from LeetCode

--- LeetCode/GRIND_100.md
+++ LeetCode/GRIND_100.md
@@ -3,7 +3,7 @@
 Track your progress on the Grind 100 coding interview preparation list.
 
 ## Progress
-- **Completed:** 6 / 100 (6.0%)
+- **Completed:** 7 / 100 (7.0%)
 
 ---
 
@@ -11,7 +11,7 @@ Track your progress on the Grind 100 coding interview preparation list.
 
 ### 📂 Array & Two Pointers
 - [x] [Two Sum](./Java/Easy/1. Two Sum/)
-- [ ] 3Sum
+- [x] [3Sum](./Java/Medium/15. 3Sum/)
 - [ ] Container With Most Water
 - [x] [Move Zeroes](./Java/Easy/283. Move Zeroes/)
 - [x] [Sort Colors](./Java/Medium/75. Sort Colors/)
---
+++ LeetCode/Java/Medium/15. 3Sum/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> triplet =Arrays.asList(nums[i] , nums[j] , nums[k]);
+                        Collections.sort(triplet);
+                        set.add(triplet);
+                        
+                    }
+                }
+            }
+        }
+        for(List<Integer> x : set){
+            result.add(x);
+        }
+        return result;
+    }
+}
