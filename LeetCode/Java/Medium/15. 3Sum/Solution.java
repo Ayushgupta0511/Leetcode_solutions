99f6dd6d1d4fbca34836b7d3297f763300d8215b
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-08-30 20:21:58 +0530

    Add Medium Java Solution for 15. 3Sum from LeetCode

--- LeetCode/Java/Medium/15. 3Sum/Solution.java
+++ LeetCode/Java/Medium/15. 3Sum/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
-                
+                for(int k=j+1;k<nums.length;k++){
                     if(nums[i] + nums[j] + nums[k] == 0){
                         List<Integer> triplet =Arrays.asList(nums[i] , nums[j] , nums[k]);
                         Collections.sort(triplet);
