805b3fe783096924cc73c03c17745498393f254f
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-01 21:21:08 +0530

    Add Easy Java Solution for 35. Search Insert Position from LeetCode

---
+++ LeetCode/Patterns/Binary Search/35. Search Insert Position/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length;
+        while(low <= high){
+            int mid= (low+high)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(target > nums[mid]){
+                low = mid+1;
+                if(low == high){
+                    return low;
+                }
+            }
+            else {
+                high = mid-1;
+                if(low == high){
+                    return low -1;
+                }
+            }
+        }
+        return -1;
+    }
+}
