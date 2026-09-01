9ae616783c662f84b686de5d78a4787cf1565936
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-01 21:26:12 +0530

    Add Easy Java Solution for 35. Search Insert Position from LeetCode

--- LeetCode/Java/Easy/35. Search Insert Position/Solution.java
+++ LeetCode/Java/Easy/35. Search Insert Position/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int low = 0;
-        int high = nums.length;
+        int high = nums.length-1;
         while(low <= high){
             int mid= (low+high)/2;
             if(target == nums[mid]){
