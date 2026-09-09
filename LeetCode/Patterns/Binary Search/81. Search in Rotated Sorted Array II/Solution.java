7443555802217f652b34fe9aeef4d1957acbbac2
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-09 22:40:14 +0530

    Add Medium Java Solution for 81. Search in Rotated Sorted Array II from LeetCode

--- LeetCode/Patterns/Binary Search/81. Search in Rotated Sorted Array II/Solution.java
+++ LeetCode/Patterns/Binary Search/81. Search in Rotated Sorted Array II/Solution.java
@@ -1,8 +1,24 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i =0;i<nums.length;i++){
-            if(target == nums[i]){
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(target == nums[mid]){
                 return true;
+            }
+            if(nums[low] <= nums[mid]){
+                high = mid-1;
+                low++;
+            }
+            else {
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid+1;
+                }
+                else {
+                    high = mid+1;
+                }
             }
         }
         return false;
