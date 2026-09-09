629244adfd948380fc66af736619998c060918b5
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-09 22:14:46 +0530

    Add Medium Java Solution for 81. Search in Rotated Sorted Array II from LeetCode

--- LeetCode/Java/Medium/81. Search in Rotated Sorted Array II/Solution.java
+++ LeetCode/Java/Medium/81. Search in Rotated Sorted Array II/Solution.java
@@ -1,8 +1,18 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i =0;i<nums.length;i++){
-            if(target == nums[i]){
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(target == nums[mid]){
                 return true;
+            }
+            if(target < nums[mid]){
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
             }
         }
         return false;
