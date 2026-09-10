4fb6f0a6232d2a3be9239987941aa6d67007a264
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-10 21:40:37 +0530

    Add Medium Java Solution for 540. Single Element in a Sorted Array from LeetCode

--- LeetCode/Java/Medium/540. Single Element in a Sorted Array/Solution.java
+++ LeetCode/Java/Medium/540. Single Element in a Sorted Array/Solution.java
@@ -9,7 +9,7 @@ class Solution {
                 ans =  nums[i];
             }
         }
-        if(nums.length ==1 ){
+        if(nums.length ==1 || nums[0] != nums[1]){
             ans = nums[0];
         }
         return ans;
