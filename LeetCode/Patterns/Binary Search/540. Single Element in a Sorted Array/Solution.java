70ddecd6bec263fe585f229348f62fb99b33cfc2
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-10 21:26:25 +0530

    Add Medium Java Solution for 540. Single Element in a Sorted Array from LeetCode

--- LeetCode/Patterns/Binary Search/540. Single Element in a Sorted Array/Solution.java
+++ LeetCode/Patterns/Binary Search/540. Single Element in a Sorted Array/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans = nums;
+        int ans = nums[nums.length-1];
         for(int i =1;i<nums.length-1;i++){
             if(nums[i-1]== nums[i] || nums[i] == nums[i+1]){
                 continue;
