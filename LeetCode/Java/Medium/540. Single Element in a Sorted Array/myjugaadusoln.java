e800c4643b2ee106f8134ac89a258050672d5c46
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-10 21:47:03 +0530

    My own solution for finding single element in array

---
+++ LeetCode/Java/Medium/540. Single Element in a Sorted Array/myjugaadusoln.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int ans = nums[nums.length-1];
+        for(int i =1;i<nums.length-1;i++){
+            if(nums[i-1]== nums[i] || nums[i] == nums[i+1]){
+                continue;
+            }
+            else {
+                ans =  nums[i];
+            }
+        }
+        if(nums.length ==1 || nums[0] != nums[1]){
+            ans = nums[0];
+        }
+        return ans;
+    }
+}
