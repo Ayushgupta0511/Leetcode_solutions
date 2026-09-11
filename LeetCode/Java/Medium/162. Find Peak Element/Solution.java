65b0ba6edf97020e11596d504d71b877511c5002
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-12 00:56:46 +0530

    Add Medium Java Solution for 162. Find Peak Element from LeetCode

--- LeetCode/Java/Medium/162. Find Peak Element/Solution.java
+++ LeetCode/Java/Medium/162. Find Peak Element/Solution.java
@@ -14,8 +14,11 @@ class Solution {
             if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
                 return ans = mid;
             }
-            if(nums[mid] < nums[mid-1] || nums[mid] < nums[mid+1]){
-                low = mid+1;
+            if(nums[mid] < nums[mid-1] || nums[mid] > nums[mid+1]){
+                high = mid-1;
+            }
+            else {
+                low = mid +1;
             }
         }
         return -1;
