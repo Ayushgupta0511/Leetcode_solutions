a92d934b26c14c5a791fa32341b771f87addb179
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-13 23:46:33 +0530

    Add Medium Java Solution for 875. Koko Eating Bananas from LeetCode

--- LeetCode/Patterns/Binary Search/875. Koko Eating Bananas/Solution.java
+++ LeetCode/Patterns/Binary Search/875. Koko Eating Bananas/Solution.java
@@ -1,24 +1,29 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int q = piles.length;
-        int count =0;
-        int max = Integer.MIN_VALUE;
-        for(int i =0;i<q;i++){
-            if(piles[i] > max){
-                max = piles[i];
-            }
+ public int bananas(int[] piles, int mid){
+    int hours = 0;{
+        for(int i =0;i<piles.length;i++){
+            hours = hours + (piles[i] + mid -1)/mid;
         }
-        for(int i =1;i<=max;i++){
-            count =0;
-                for(int w =0;w<q;w++){
-                    count = count + (piles[w] + i -1 ) /i;
-                }
-                if(count <= h){
-                    return i;
-                }
-            
-        }
-        return 0;
+        return hours;
     }
-    
+ }
+    public int minEatingSpeed(int[] piles, int h) {
+        int low =1;
+        int high = Arrays.stream(piles).max().getAsInt();
+        int ans = Integer.MAX_VALUE;
+        int hours =0;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            hours = bananas(piles , mid);
+            if( hours <= h ){
+                ans = mid;
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+
+        }
+        return ans;
+    }
 }
