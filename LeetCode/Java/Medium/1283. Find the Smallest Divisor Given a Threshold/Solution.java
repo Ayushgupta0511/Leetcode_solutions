e6fd0f9b09d233c58b4d45d0a78f3ac86e64405f
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-14 21:39:33 +0530

    Add Medium Java Solution for 1283. Find the Smallest Divisor Given a Threshold from LeetCode

--- LeetCode/Java/Medium/1283. Find the Smallest Divisor Given a Threshold/Solution.java
+++ LeetCode/Java/Medium/1283. Find the Smallest Divisor Given a Threshold/Solution.java
@@ -1,16 +1,20 @@
 class Solution {
-    // static int divide(int[] nums){
-    //     int sum =0;
-    //     for(int i =0;i<nums.length;i++){
-    //         sum = sum + (nums[i] + 4) /5;
-    //     }
-    //     return sum;
-    // }
-    public int smallestDivisor(int[] nums, int threshold) {
+    static int divide(int[] nums){
         int sum =0;
         for(int i =0;i<nums.length;i++){
-            sum = sum + (nums[i] + 4) /5;
+            sum = sum + (nums[i] + mid -1) /mid;
         }
         return sum;
+    }
+    public int smallestDivisor(int[] nums, int threshold) {
+        int low =0;
+        int high = Arrays.stream(nums).max().getAsInt();
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            divide(nums , mid);
+            if()
+
+        }
+        
     }
 }
