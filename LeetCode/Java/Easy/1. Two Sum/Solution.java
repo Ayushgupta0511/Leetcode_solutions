02627f5142373457ecb6a6de1d86bd53a56b5298
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-20 19:44:40 +0530

    Add Easy Java Solution for 1. Two Sum from LeetCode

--- LeetCode/Java/Easy/1. Two Sum/Solution.java
+++ LeetCode/Java/Easy/1. Two Sum/Solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int sum =0;
         int[] arr = new int[2];
         outer:
-        for(int i =0;i < nums.length ;i++){
-            for(int j = i+1;j<nums.length;j++){
-                sum =0;
-                sum = nums[i] + nums[j];
-                if(sum == target){
-                    arr[0] = i;
-                    arr[1] = j;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    arr[0] =i;
+                    arr[1] =j;
                     break outer;
                 }
             }
         }
         return arr;
     }
+    
 }
